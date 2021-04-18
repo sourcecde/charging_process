99e21eafee71fee41e140afa90ad8745d03a87a1
Matal <dbrj.prof@gmail.com>
2021-04-18 16:33:24 +0200

    first commit

---
+++ app/Http/Controllers/ChargeCalculationController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DateTime;
+
+class ChargeCalculationController extends Controller
+{
+
+    public function getTime($time)
+    {
+        $d = new DateTime($time);
+        $d->format('Y-m-d\TH:i:s.u'); // 2016-10-06T09:50:54.000000
+        return $d->format('Y-m-d h:i:s'); // 2016-10-06 09:50.00
+    }
+
+    public function differenceInMinutes($startdate, $enddate)
+    {
+        $t1 = strtotime(str_replace('/', '-', $startdate));
+        $t2 = strtotime(str_replace('/', '-', $enddate));
+        return abs($t1 - $t2) / 60;
+    }
+
+    function store()
+    {
+
+        $body = file_get_contents('php://input');
+        $object = json_decode($body, true);
+        $new_arr = is_array($object) ? array_values($object) : array();
+
+        $allReading = array_reduce($new_arr, 'array_merge', array());
+
+        $energy = $allReading["energy"];
+        $time = $allReading["time"];
+        $transaction = $allReading["transaction"];
+        $meterStart = $allReading["meterStart"];
+        $meterStop = $allReading["meterStop"];
+        $timestampStart = $allReading["timestampStart"];
+        $timestampStop = $allReading["timestampStop"];
+
+
+        $this->meterStart = $meterStart;
+        $this->meterStop = $meterStop;
+        $this->timestampStart = $timestampStart;
+        $this->timestampStop = $timestampStop;
+        $this->energy = $energy;
+        $this->transaction = $transaction;
+
+        $timePrice = round($this->differenceInMinutes($this->getTime($this->timestampStart),
+                $this->getTime($this->timestampStop)) * $time / 60, 3);
+
+        $energyPrice = round(((($this->meterStop - $this->meterStart) / 1000) * $energy), 3);
+
+        $cdr = round(($timePrice + $energyPrice + $transaction), 2);
+
+        $data = array(
+            'overall' => $cdr,
+            "components" => array(
+                "energy" => $energyPrice,
+                "time" => $timePrice,
+                "transaction" => $transaction
+            )
+        );
+        echo json_encode($data);
+        exit;
+    }
+}
