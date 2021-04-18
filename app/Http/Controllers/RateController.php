b25ce7b6197ecb094b3c975d22f04539ef1fc3bf
Matal <dbrj.prof@gmail.com>
2021-04-19 00:07:13 +0200

    view create and get json value

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DateTime;
 
-class ChargeCalculationController extends Controller
+class RateController extends Controller
 {
+    public function index()
+    {
+        return view('rate');
+    }
 
     public function getTime($time)
     {
@@ -22,10 +26,27 @@ class ChargeCalculationController extends Controller
         return abs($t1 - $t2) / 60;
     }
 
-    function store()
-    {
 
-        $body = file_get_contents('php://input');
+    function store(Request $request)
+    {
+        if($request->ajax()){
+        $input = $request->all();
+        $energy = $_POST['energy'];
+     $time = $_POST['time'];
+     $transaction = $_POST['transaction'];
+     $timestampStart = $_POST['timestampStart'];
+     $timestampStop = $_POST['timestampStop'];
+     $meterStart = $_POST['meterStart'];
+     $meterStop = $_POST['meterStop'];
+
+     $array = array("rate" => array("energy" => $energy,  
+              "time" => $time,"transaction" => $transaction),
+              "cdr" => array( "meterStart" => $meterStart, "timestampStart" => $timestampStart,
+                                "meterStop" => $meterStop, "timestampStop" => $timestampStop ));
+     $body = json_encode( $data = $array );
+ }
+ else{$body = file_get_contents('php://input');}
+
         $object = json_decode($body, true);
         $new_arr = is_array($object) ? array_values($object) : array();
 
---
+++ resources/views/rate.blade.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Coding Challenge</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/additional-methods.min.js"></script>
+  <style>
+   .error{ color:red; } 
+  </style>
+</head>
+<body>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6 mt-3 offset-md-3">
+            <div class="card">
+               <div class="card-header bg-dark text-white">
+                  <h6>Charging Process</h6>
+               </div>
+               <div class="card-body">
+                  <form id="post-form" method="post" action="javascript:void(0)">
+                     @csrf
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="alert alert-success d-none" id="msg_div">
+                                   <span id="res_message"></span>
+                              </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Energy<span class="text-danger">*</span></label>
+                              <input type="text" name="energy" placeholder="Enter Energy" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('energy') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Time Rate<span class="text-danger">*</span></label>
+                              <input type="text" name="time" placeholder="Enter Time Rate" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('time') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Transaction<span class="text-danger">*</span></label>
+                              <input type="text" name="transaction" placeholder="Enter Transaction" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('transaction') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Time Start<span class="text-danger">*</span></label>
+                              <input type="text" name="timestampStart" placeholder="Enter Time Start" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('timestampStart') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Time End<span class="text-danger">*</span></label>
+                              <input type="text" name="timestampStop" placeholder="Enter Time End" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('timestampStop') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Meter Start<span class="text-danger">*</span></label>
+                              <input type="text" name="meterStart" placeholder="Enter Meter Start" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('meterStart') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="form-group">
+                              <label>Enter Meter End<span class="text-danger">*</span></label>
+                              <input type="text" name="meterStop" placeholder="Enter Meter End" class="form-control">
+                              <span class="text-danger p-1">{{ $errors->first('meterStop') }}</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <button type="submit" id="send_form" class="btn btn-block btn-success">Submit</button>
+                        </div>   
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</body>
+<script>
+   if ($("#post-form").length > 0) {
+    $("#post-form").validate({
+      
+    rules: {
+      energy: {
+        required: true,
+      },
+      time: {
+        required: true,
+      },
+      transaction: {
+        required: true,
+      },
+      timestampStart: {
+        required: true,
+      },
+      timestampStop: {
+        required: true,
+      },
+      meterStart: {
+        required: true,
+      },
+      meterStop: {
+        required: true,
+      },
+    },
+    messages: {
+      energy: {
+        required: "Please Enter Energy",
+      },
+      time: {
+        required: "Please Enter Time",
+      },
+      transaction: {
+        required: "Please Enter Transaction",
+      },
+      timestampStart: {
+        required: "Please Enter Time Start",
+      },
+      timestampStop: {
+        required: "Please Enter Time Stop",
+      },
+      meterStart: {
+        required: "Please Enter Meter Start",
+      },
+      meterStop: {
+        required: "Please Enter Meter Stop",
+      },
+    },
+    submitHandler: function(form) {
+     // $.ajaxSetup({
+     //      headers: {
+     //          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+     //      }
+     //  });
+      $('#send_form').html('Sending..');
+      $.ajax({
+        url: '/rate' ,
+        type: "POST",
+        data: $('#post-form').serialize(),
+        success: function( response ) {
+            console.log(response);
+            $('#send_form').html('Submit');
+            $('#res_message').show();
+            $('#res_message').html(response);
+            $('#msg_div').removeClass('d-none');
+            $(document).ready(function(){
+    $(this).scrollTop(0);
+});
+ 
+            document.getElementById("post-form").reset(); 
+            setTimeout(function(){
+            $('#res_message').hide();
+            $('#msg_div').hide();
+            },10000);
+        }
+      });
+    }
+  })
+}
+</script>
+</html>
+<!-- <form action="" method="POST">
+<label>Enter Energy:</label><br />
+<input type="text" name="energy" placeholder="Enter Energy" required/>
+<br /><br />
+<label>Enter Time Rate:</label><br />
+<input type="text" name="time" placeholder="Enter Time Rate" required/>
+<br /><br />
+<label>Enter Transaction:</label><br />
+<input type="text" name="transaction" placeholder="Enter Transaction" required/>
+<br /><br />
+<label>Enter Time Start:</label><br />
+<input type="text" name="timestampStart" placeholder="Enter Time Start" required/>
+<br /><br />
+<label>Enter Time End:</label><br />
+<input type="text" name="timestampStop" placeholder="Enter Time End" required/>
+<br /><br />
+<label>Enter Meter Start:</label><br />
+<input type="text" name="meterStart" placeholder="Enter Meter Start" required/>
+<br /><br />
+<label>Enter Meter Stop:</label><br />
+<input type="text" name="meterStop" placeholder="Enter Meter Stop" required/>
+<br /><br />
+<button type="submit" name="submit">Submit</button>
+</form> -->
